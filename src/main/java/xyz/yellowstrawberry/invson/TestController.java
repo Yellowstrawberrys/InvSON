baaebec78ace5f2580eedd6930fa31b4cd7122ff
Yellowstrawberrys <dirjhhsg@gmail.com>
2023-12-09 22:37:18 +0900

    update

---
+++ src/main/java/xyz/yellowstrawberry/invson/TestController.java
@@ -0,0 +1,11 @@
+package xyz.yellowstrawberry.invson;
+
+import xyz.yellowstrawberry.invson.annotations.InteractionEvent;
+
+public class TestController {
+
+    @InteractionEvent("wow")
+    public void onInteraction() {
+
+    }
+}
---
+++ src/main/java/xyz/yellowstrawberry/invson/annotations/InteractionEvent.java
@@ -0,0 +1,5 @@
+package xyz.yellowstrawberry.invson.annotations;
+
+public @interface InteractionEvent {
+    String value();
+}
